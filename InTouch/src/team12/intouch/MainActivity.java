a7b721d5949b15f9ef73c7ad5997759e18155d63
qiulug <qiulug@andrew.cmu.edu>
2014-04-30 01:06:59 -0400

    caching added

--- InTouch/src/team12/intouch/MainActivity.java
+++ InTouch/src/team12/intouch/MainActivity.java
@@ -3,6 +3,7 @@ package team12.intouch;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,8 +11,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseUser;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -20,11 +23,16 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Parse.initialize(this, "gdx6O89iizP2Jg0ibFBfW9Df27o4lA9WCXuQDf2c", "djP9wbuJnul2UIcnwyQU5t69JN4CmleqYLZfWOG5");
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new Login_fragment()).commit();
+		if (ParseUser.getCurrentUser() != null) {
+			  // Start an intent for the logged in activity
+			  startActivity(new Intent(this, TabActivity.class));
+			} else {
+			  // Start and intent for the logged out activity
+		  if (savedInstanceState == null) {
+			    getSupportFragmentManager().beginTransaction()
+					  .add(R.id.container, new Login_fragment()).commit();
 		}
+	   }
 	}
 
 	@Override
